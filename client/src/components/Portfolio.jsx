0db99f6c1cf39a5396c46035099c0a1b58998b43
Nasghul01 <nasrul01arsyad@gmail.com>
2024-03-19 23:21:09 +0800

    update better code

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,27 +1,57 @@
 import React from "react";
-
-import Project from "./Project.jsx";
+import Video1 from "./Video_FoundIt.mp4";
 import ProjectData from "./ProjectData.js"
 
 export default function Portfolio () {
-    const ProjectElement = ProjectData.map(project => {
-        return <Project 
-                key={project.id}
-                id = {project.id}
-                title={project.title} 
-                role={project.role} 
-                description={project.description}
-                path = {project.path}/>
-    })
+
+    const ProjectElements = ProjectData.map(project => (
+        <div className="container-FoundIt px-0 row align-items-center justify-content-between mb-5"
+        key={project.id}>
+            <div className="container col-md pe-5">
+                    <h1 className="mb-5">{project.title}</h1>
+                    <h4 className="mb-2">{project.role}</h4>
+                    <p className="text-dark lead">{project.description}</p>
+                    {project.id === 1 ? 
+                    <>
+                    </>
+                    : <a href={project.link}>
+                        <button className="btn btn-lg bg-warning mb-5">Click Me!</button>
+                    </a>}
+            </div>
+            <div className="cointaner col-md">
+                {project.id === 1 ?
+                <>
+                    <div className="phone-frame">
+                        <div className="screen">
+                            <video autoPlay controls className="Vid-FoundIt">
+                            <source src={Video1} type="video/mp4"/>
+                            Your browser does not support the video tag.
+                            </video>
+                        </div>
+                    </div>
+
+                </> :
+                <>
+                    <div className="container col-md">
+                        {project.pictures && project.pictures.map((picture,index) => (
+                            <div key={index} className="container row mb-3">
+                                <img src={picture} className="d-block w-100" alt={picture}/>
+                            </div>
+                        ))}
+
+                        </div>   
+
+                </>}
+            </div>
+        </div>
+    ))
 
     return (
         <section className="p-5 bg-secondary-light " id="Portfolio">
                 <div className="container">
                     <h2 className="text-center mb-5">Portfolio</h2>
-                        {ProjectElement}
-
-                    
+                        {ProjectElements}
                 </div>
         </section>
     )
-}
+ }
--- client/src/components/ProjectData.js
+++ client/src/components/ProjectData.js
@@ -4,19 +4,37 @@ export default [
         title: "Found It! Object Tracker",
         role: "Full stack developer | UI Designer",
         description: "A mobile application that can track misplaced items using the Bluetooth technology.It also capable to share the coordinate of misplaced item to their family through Telegram. It also can share details of location, number phone to public if you are missing your tag and cannot be found, the community can contact you if they found it.",
-        path: "./Video_FoundIt.mp4"
+        path: "./Video_FoundIt.mp4",
+        link: "",
+        pictures : [""]
+        
     }, 
-    // {
-    //     id: 2,
-    //     title: "Note Taker Web App",
-    //     role: "Front end developer | UI Designer",
-    //     description: "A note app that can add many note , delete , filter and more other feature",
-    //     path: ""
-    // },
     {
-        id: 3,
+        id: 2,
         title: "Chicken Royale website",
         role: "Full stack developer | UI Designer",
-        description: "A restaurant website application for user to do reservation of the table in the restaurant. Also have feedback about the food or the restaurant and customer service by star and comment. "
+        description: "A restaurant website application for user to do reservation of the table in the restaurant. Also have feedback about the food or the restaurant and customer service by star and comment. ",
+        link: "http://localhost/GitHub/ICT600/Login-Register/login.php",
+        pictures : ["/images/ulasan.png","/images/borang_tempahan.png"]
+        
+    },
+    {
+        id: 3,
+        title: "NotesWrite",
+        role: "Front end developer | UI Designer",
+        description: "Notes is important to write because sometimes we are forgot thing.This notes app provide some basic features such as taking note and delete note.",
+        path: "",
+        link: "http://localhost:5174/",
+        pictures : ["/images/Note-app-preview.png"]
+
+    },
+    {
+        id: 4,
+        title: "Tenzies Game",
+        role: "Front end developer | UI Designer",
+        description: "Tenzi is a fast-paced dice game where players need to roll their dice to match a chosen number.Quick reflexes and a bit of luck are key to winning this thrilling game of speed and strategy",
+        path: "",
+        link: "https://tenzies-dicegame-nas.netlify.app/",
+        pictures : ["/images/tenzies.png"]
     }
 ]
